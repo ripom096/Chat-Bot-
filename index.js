c5f6033a6c2784fe4c58b0d78569baaf4960b13a
ripom096 <riponahammed078@gmail.com>
2026-02-02 01:57:22 +0600

    Set up Express server with basic route

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is Alive!'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 const login = require("fca-unofficial");
 const fs = require("fs");
 const axios = require("axios");
